556aee9f4ba943454f7229591334244a854ac541
Nick Topacio <topacio.3@osu.edu>
2023-12-14 20:09:34 -0500

    Work on day5

--- 2023/python/day5.py
+++ 2023/python/day5.py
@@ -1,18 +1,51 @@
 import sys
 
-def main():
+def parse_line(line, map_range):
+    print(line)
+    
+def parse_seed_map(line, map_range):
+    seed_nums = (line.split(" "))[1:]
+    seed_array = [int(seed_num) for seed_num in seed_nums]
+    map_range['seeds'] = seed_array
+
+def parse_map(lines):
+    map_range = {}
+    parse_seed_map(lines[0].strip(), map_range)
+    lines = lines[1:] # remove first seed line
+    for line in lines:
+        if line.strip() != "":
+            parse_line(line.strip(), map_range)
+
+
+def parse_file():
     # open text file
     in_file_name = sys.argv[1] # 0 = script, 1 = file name
     input_file = open(in_file_name, 'r')
-    lines = input_file.readlines()
-
-    # print file
-    for line in lines:
-        print(line.strip())
-
+    text = input_file.readlines()
     # close file
     input_file.close()
+    return text
+
+def main():
+    lines = parse_file()
+    # parse seed file
+    parse_map(lines)
 
 if __name__ == "__main__":
     main()
 
+# seeds: 79 14 55 13
+#
+#   range: 2
+#   src: 98 - 99
+#   dest: 50 - 51 
+#
+#   range: 48
+#   src: 50 - 97
+#   dest: 52 - 99
+#
+#   everything else:
+#   src: 0 - 49
+#   dest: 0 - 49
+#  
+# seed-to-soil: 79->81, 14->14, 55->57, 13->13
